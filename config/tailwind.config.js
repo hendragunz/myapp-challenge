459091d35542d1449b8a72a3e5521f778f19174c
Hendra Gunawan <hendragunz@codecampz.com>
2025-01-11 00:55:57 +0700

    setup basic css tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,15 +2,21 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      zIndex: {
+        "-1": "-1",
+      },
+      flexGrow: {
+        5: "5",
       },
     },
   },
@@ -18,5 +24,5 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
