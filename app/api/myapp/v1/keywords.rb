07d20366d0a2fd179adaf0b2f61a032d3d60ffee
Hendra Gunawan <hendragunz@codecampz.com>
2025-01-15 08:21:47 +0700

    simple API to upload keywords

--- app/api/myapp/v1/keywords.rb
+++ app/api/myapp/v1/keywords.rb
@@ -4,11 +4,45 @@ module Myapp
       error!("401 Unauthorized", 401) unless authenticated
     end
 
+    helpers do
+      def upload_keywords_params
+        declared(params)[:keyword]
+      end
+    end
+
     resources :keywords do
       desc "Return all keywords"
       get do
         @keywords = current_user.keywords.newest
         present @keywords, with: Entities::KeywordEntity
+      end
+
+      desc "To create keywords from uploaded file"
+      params do
+        requires :keyword, type: Hash do
+          requires :file, type: File#, content_type: ['text/plain", "application/csv']
+        end
+      end
+      post do
+        begin
+          error!("Uploaded file must by CSV", 400) unless upload_keywords_params[:file][:type] == 'text/csv'
+          parser = KeywordParser.new(current_user, upload_keywords_params[:file][:tempfile])
+
+          error!("Uploaded file must have keywords less or equal than 100 rows", 400) if parser.keywords.length > 100
+          error!("Uploaded file must have at least 1 row", 400) if parser.keywords.blank?
+
+          new_keywords = parser.new_keywords
+
+          if new_keywords.present?
+            @keywords = new_keywords.map{|rec| current_user.keywords.create(name: rec)}
+            present @keywords, with: Entities::KeywordEntity
+          else
+            error!("Unable to see new keywords", 422)
+          end
+
+        rescue CSV::InvalidEncodingError => e
+          error!("Bad CSV file, please double check again", 400)
+        end
       end
 
 
